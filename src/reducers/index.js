28e6e4ff32c2b8126d44afe7803a74cbf7450378
Tiago Almeida <tyagow@hotmail.com.br>
2018-06-16 05:50:12 -0300

    feat: new rootReducer with questions and answers reducers combined

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
-import questions from "./questionReducer";
+import questionCreator from "./questionCreatorReducer";
+import answers from "./answersReducer";
 
 export default combineReducers({
-  questions
+  questionCreator,
+  answers
 });
